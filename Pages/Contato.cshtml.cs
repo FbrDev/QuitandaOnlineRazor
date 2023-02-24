37fc7a5639c2bac247bb59d69a026f0a10b9910a
fpcmaria@apoioprodesp.sp.gov.br <fpcmaria@apoioprodesp.sp.gov.br>
2023-02-24 20:19:06 -0300

    Página Modelo e Quatro Novas Páginas

---
+++ Pages/Contato.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuitandaOnline.Pages
+{
+    public class ContatoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
